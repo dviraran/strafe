f77f0f61bd4716367a9d1c9145452979ba22558c
moshe2590 <70953663+moshe2590@users.noreply.github.com>
2023-03-16 10:46:04 +0200

    Update embedding_utils.py

--- Utils/embedding_utils.py
+++ Utils/embedding_utils.py
@@ -44,67 +44,6 @@ def train_embedding(featureSet, feature_matrix_3d_transpose, window_days, person
     model.wv.save(embedding_filename)
     return embedding_filename
 
-
-def plot_embedding(model_filename):
-    from sklearn.decomposition import IncrementalPCA  # inital reduction
-    from sklearn.manifold import TSNE  # final reduction
-    import numpy as np  # array handling
-    import matplotlib.pyplot as plt
-    import random
-    model = Word2Vec.load(model_filename)
-    num_dimensions = 2
-    vectors = np.asarray(model.wv.vectors)
-    labels = np.asarray(model.wv.index_to_key)
-    tsne = TSNE(n_components=num_dimensions, random_state=0)
-    vectors = tsne.fit_transform(vectors)
-    x_vals = [v[0] for v in vectors]
-    y_vals = [v[1] for v in vectors]
-    random.seed(0)
-    plt.figure(figsize=(12, 12))
-    plt.scatter(x_vals, y_vals)
-    indices = list(range(len(labels)))
-    selected_indices = random.sample(indices, 25)
-    for i in selected_indices:
-        plt.annotate(labels[i], (x_vals[i], y_vals[i]))
-    plt.savefig(f"{model_filename}.png")
-
-
-def closest_embedding(model_filename, featureSet, all_concept_map_reverse):
-    all_concept_map = {v: k for k, v in all_concept_map_reverse.items()}
-
-    from sklearn.decomposition import IncrementalPCA  # inital reduction
-    from sklearn.manifold import TSNE  # final reduction
-    import numpy as np  # array handling
-    model = Word2Vec.load(model_filename)
-    vectors = np.asarray(model.wv.vectors)
-    labels = np.asarray(model.wv.index_to_key)
-    n = len(labels)
-    #distances_table = np.zeros((n, n))
-    #ltn = {l:i for l,i in zip (labels, list(range(n)))}
-    #ntl = {i:l for l,i in zip (labels, list(range(n)))}
-    #for v1, l1 in zip(vectors, labels):
-    #    for v2, l2 in zip(vectors, labels):
-    #        if l1 == l2: distances_table[ltn[l1]][ltn[l2]] = 10*6
-    #        distances_table[ltn[l1]][ltn[l2]] = np.linalg.norm(v1-v2)
-    #closests = distances_table.argmin(axis=1)
-    #for label in labels:
-    #    print(f"The closest diagnosis to {label} is {ntl[closests[ltn[label]]]} ")
-    #print(model.wv.index_to_key)
-    #print()
-    print(all_concept_map)
-    #wv = KeyedVectors.load(model_filename, mmap='r')
-    print(model.wv.index_to_key)
-
-    print(all_concept_map_reverse)
-    for concept, index in all_concept_map_reverse.items():
-        if concept in [443601, 443597,443612, 443611,81902,133810,193782,201826, 40482801,443732,443731,313217,42872402,437827,313217]:
-            sims = model.wv.most_similar(str(index), topn=10)
-            print(f"The closest diagnoses to {concept} are {all_concept_map[int(sims[0][0])]} and {all_concept_map[int(sims[1][0])]} ")
-
-
-
 if __name__ == '__main__':
     pass
-    # embedding_filename = "wtv_data_window_90d_20epochs_wv_big100"
-    # plot_embedding(f"{embedding_filename}_model")
-    # plot_embedding("wtv_data_window_90d_20epochs_model")
+   
